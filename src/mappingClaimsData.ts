5a734508c8290222bb3ed27fe94af99e8c52311b
Daniel Heyman <daniel.heyman@gmail.com>
2019-12-22 12:45:30 -0500

    Add claim and vote verdicts and additional info

--- src/mappingClaimsData.ts
+++ src/mappingClaimsData.ts
@@ -1,32 +1,44 @@
-import { VoteCast, ClaimRaise } from "../generated/templates/ClaimsData/ClaimsData"
-import {Vote, Claim} from "../generated/schema"
-import{isLatestNexusContract, getUser} from "./helpers";
+import { VoteCast, ClaimRaise, ClaimsData } from "../generated/templates/ClaimsData/ClaimsData"
+import { Vote, Claim } from "../generated/schema"
+import{ isLatestNexusContract, getUser } from "./helpers";
 
 export function handleVoteCast(event: VoteCast): void {
   if(isLatestNexusContract("claimsData", event.address)) {
+    let user = getUser(event.params.userAddress);
+    user.voteCount += 1;
+    user.save();
+
+    let claim = Claim.load(event.params.claimId.toString());
+    claim.voteCount += 1;
+    claim.save();
+
     let id = event.params.claimId.toString() + "-" + event.params.userAddress.toHexString();
-    let entity = Vote.load(id);
-    if(entity == null) {
-      entity = new Vote(id);
-      entity.user = getUser(event.params.userAddress).id;
-      entity.claim = event.params.claimId.toString();
-      entity.verdict = event.params.verdict;
-      entity.submitDate = event.params.submitDate;
-      entity.save();
-    }
+    let entity = new Vote(id);
+    entity.user = user.id;
+    entity.claim = claim.id;
+    entity.verdict = event.params.verdict == 1 ? "Accepted" : "Denied";
+    entity.submitDate = event.params.submitDate;
+    entity.save();
   }
 }
 
 export function handleClaimRaise(event: ClaimRaise): void {
   if(isLatestNexusContract("claimsData", event.address)) {
+    let cd = ClaimsData.bind(event.address);
+
+    let user = getUser(event.params.userAddress);
+    user.claimCount += 1;
+    user.save();
+
     let id = event.params.claimId.toString();
-    let entity = Claim.load(id);
-    if(entity == null) {
-      entity = new Claim(id);
-      entity.user = getUser(event.params.userAddress).id;
-      entity.cover = event.params.coverId.toString();
-      entity.submitDate = event.params.dateSubmit;
-      entity.save();
-    }
+    let entity = new Claim(id);
+    entity.user = user.id;
+    entity.cover = event.params.coverId.toString();
+    entity.submitDate = event.params.dateSubmit;
+    entity.statusUpdateDate = event.params.dateSubmit;
+    entity.status = "CAVote";
+    entity.voteCount = 0;
+    entity.maxVotingTime = cd.maxVotingTime();
+    entity.save();
   }
 }
--- src/mappingTokenData.ts
+++ src/mappingTokenData.ts
@@ -6,23 +6,19 @@ import { BigInt } from "@graphprotocol/graph-ts";
 export function handleAddStake(call: AddStakeCall): void {
   if (isLatestNexusContract("tokenData", call.to)) {
     let user = getUser(call.inputs._stakerAddress);
-    let id = call.inputs._stakerAddress.toHexString() + "-" + BigInt.fromI32(user.stakeCount).toString();
+    user.stakeCount += 1;
+    user.save();
 
-    let entity = Stake.load(id);
-    if (entity == null) {
-      entity = new Stake(id);
-      entity.user = user.id;
-      entity.contract = getInsuredContract(call.inputs._stakedContractAddress).id;
-      entity.amount = toTokenDecimals(call.inputs._amount);
-      entity.unlockedAmount = BigInt.fromI32(0).toBigDecimal();
-      entity.burntAmount = BigInt.fromI32(0).toBigDecimal();
-      entity.daysToStake = 250;
-      entity.created =  call.block.timestamp;
-      entity.expires = call.block.timestamp.plus(BigInt.fromI32(entity.daysToStake * 24 * 60 * 60));
-      entity.save();
-
-      user.stakeCount += 1;
-      user.save();
-    }
+    let id = call.inputs._stakerAddress.toHexString() + "-" + BigInt.fromI32(user.stakeCount - 1).toString();
+    let entity = new Stake(id);
+    entity.user = user.id;
+    entity.contract = getInsuredContract(call.inputs._stakedContractAddress).id;
+    entity.amount = toTokenDecimals(call.inputs._amount);
+    entity.unlockedAmount = BigInt.fromI32(0).toBigDecimal();
+    entity.burntAmount = BigInt.fromI32(0).toBigDecimal();
+    entity.daysToStake = 250;
+    entity.created =  call.block.timestamp;
+    entity.expires = call.block.timestamp.plus(BigInt.fromI32(entity.daysToStake * 24 * 60 * 60));
+    entity.save();
   }
 }
